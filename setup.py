f235d6cacc4034c3ea5e704838c12e1c93c37626
root <root@CentOS7.x-StagDocker.1.78>
2022-06-28 08:27:04 +0700

    change

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ except ImportError:
     from distutils.core import setup
 
 from email.utils import parseaddr
-import defa_safe
+import safe
 
-author, author_email = parseaddr(defa_safe.__author__)
+author, author_email = parseaddr(safe.__author__)
 
 
 def fread(filename):
@@ -24,13 +24,14 @@ def fread(filename):
 
 
 setup(
-    name='defa_safe',
-    version= '0.5',
+    name='Safe',
+    version=safe.__version__,
     author=author,
     author_email=author_email,
     url='https://github.com/lepture/safe',
-    packages=["defa_safe"],
+    packages=["safe"],
     description="Is your password safe?",
+    zip_safe=False,
     include_package_data=True,
     platforms='any',
     long_description=fread('README.rst'),
--- test.py
+++ test.py
@@ -2,64 +2,64 @@
 import os
 import tempfile
 
-import defa_safe
+import safe
 
 def test_asdf():
-    s = defa_safe.check('dfghjkl', length=7)
+    s = safe.check('dfghjkl', length=7)
     assert not s
     assert 'pattern' in str(s)
 
 
 def test_step():
-    s = defa_safe.check('abcdefg', length=7)
+    s = safe.check('abcdefg', length=7)
     assert not s
     assert 'pattern' in s.message
 
 
 def test_common():
-    s = defa_safe.check('password')
+    s = safe.check('password')
     assert not s
     assert 'common' in s.message
 
 
 def test_short():
-    s = defa_safe.check('1')
+    s = safe.check('1')
     assert not s
     assert 'short' in s.message
 
 
 def test_simple():
-    s = defa_safe.check('yhnolku', length=6)
+    s = safe.check('yhnolku', length=6)
     assert not s
     assert 'simple' in s.message
 
 
 def test_medium():
-    s = defa_safe.check('yhnolkuT', length=7)
+    s = safe.check('yhnolkuT', length=7)
     assert not s
     assert repr(s) == 'medium'
 
 
 def test_strong():
-    s = defa_safe.check('yhnolkuT.')
+    s = safe.check('yhnolkuT.')
     assert bool(s)
     assert 'perfect' in s.message
 
 
 def test_no_cache_on_load_words():
     cache_file = _clear_cache_file()
-    words = defa_safe._load_words(cache_words=False)
+    words = safe._load_words(cache_words=False)
     assert words
     assert not os.path.exists(cache_file)
 
 
 def test_no_cache_on_safe_check():
     cache_file = _clear_cache_file()
-    s = defa_safe.check('yhnolkuT.', cache_words=False)
+    s = safe.check('yhnolkuT.', cache_words=False)
     assert not os.path.exists(cache_file)
 
 def _clear_cache_file():
-    filename = 'safe-%s.words.cache' % defa_safe.__version__
+    filename = 'safe-%s.words.cache' % safe.__version__
     _cache_file = os.environ.get(
         'PYTHON_SAFE_WORDS_CACHE',
         os.path.join(tempfile.gettempdir(), filename),
